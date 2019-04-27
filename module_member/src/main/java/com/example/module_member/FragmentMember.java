cd6363c99d21d554ab403d75464e41a130a8f01e
1789780841@qq.com <Xuyijie19971016>
2019-04-27 14:29:22 +0800

    新增主界面fragment

--- module_member/src/main/java/com/example/module_member/FragmentMember.java
+++ module_member/src/main/java/com/example/module_member/FragmentMember.java
@@ -1,10 +1,20 @@
 package com.example.module_member;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.module_base.base.BaseFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class FragmentMember extends BaseFragment {
+
+
     @Override
     public int initLayout() {
         return R.layout.fragment_member;
@@ -19,4 +29,5 @@ public class FragmentMember extends BaseFragment {
     public void initData() {
 
     }
+
 }
