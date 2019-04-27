59cfd0156645156d1f14332fd8aad197a66aca6f
SUHONGJIN <903502825@qq.com>
2019-04-27 15:35:08 +0800

    第一次提交。测试

--- module_home/src/main/java/com/example/module_home/HomeFragment.java
+++ module_home/src/main/java/com/example/module_home/HomeFragment.java
@@ -4,7 +4,21 @@ import android.view.View;
 
 import com.example.module_base.base.BaseFragment;
 
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+
 public class HomeFragment extends BaseFragment {
+
+    @BindView(R2.id.tv_title)
+    Text mg_title;
+//    @BindView(R2.id.mg_title)
+//    MagicIndicator mg_title;
+    
+
     @Override
     public int initLayout() {
         return R.layout.fragment_home;
